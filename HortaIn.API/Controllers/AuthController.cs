1e78db0552358144367da524184856ef316bf733
Lucca <luccazvps@gmail.com>
2023-03-09 23:32:25 -0300

    fix: register returning badRequest even when request succeeds

--- HortaIn.API/Controllers/AuthController.cs
+++ HortaIn.API/Controllers/AuthController.cs
@@ -46,9 +46,9 @@ namespace HortaIn.API.Controllers
                 foreach (IdentityError error in result.Errors)
                 {
                     dictionary.AddModelError(error.Code, error.Description);
+                    return new BadRequestObjectResult(new { Message = "Registro de usuário falhou", Errors = dictionary });
                 }
 
-                return new BadRequestObjectResult(new { Message = "Registro de usuário falhou", Errors = dictionary });
             }
 
             return Ok(new { Message = "Usuário registrado com sucesso" });
